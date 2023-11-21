62c979f3558ee2149533841f6391d27a4d092ada
Prashant <123720428+prashant334434@users.noreply.github.com>
2023-11-21 11:57:14 +0530

    Fc

---
+++ src/markup/components/DownloadButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const DownloadButton = ({ groupType, buttonType }) => {
+    return (
+        <div className={`download-button-group ${groupType}`}>
+            <a href="#!" className={`download-button download-button-google ${buttonType}`}>
+                <div className="download-button-inner">
+                    <div className="download-button-icon c-purple">
+                        <i className="fab fa-google-play"></i>
+                    </div>
+                    <div className="download-button-content">
+                        <h5 className="c-grey upper ls-1">get it on</h5>
+                        <h3 className="c-dark ls-2">Google Play</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="#!" className={`download-button download-button-apple ${buttonType}`}>
+                <div className="download-button-inner">
+                    <div className="download-button-icon c-pink">
+                        <i className="fab fa-apple"></i>
+                    </div>
+                    <div className="download-button-content">
+                        <h5 className="c-grey upper ls-1">get it on</h5>
+                        <h3 className="c-dark ls-2">Apple Store</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+    );
+};
+
+export default DownloadButton;
